25cef613f882c977369d18b8eedce3ab5d59f7eb
Menarg <erik.hahn.nilsson@gmail.com>
2023-12-14 18:24:02 +0100

    listan printas ut

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,19 @@
-import { collection, getDocs, addDoc, query, where, updateDoc, increment, doc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { collection, getDocs, addDoc, deleteDoc, query, where, updateDoc, increment, doc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { db } from "./modules/firebase.js";
 
-const addMovieButton = document.querySelector('#searchbar-button');
-const addMovieText = document.querySelector('#searchbar-input');
+// where info for database comes from
+const addMovieButton = document.querySelector('#add-movie_button');
+const addMovieTitle = document.querySelector('#add-movie_title');
+const addMovieGenre = document.querySelector('#add-movie_genre');
+const addMovieRelease = document.querySelector('#add-movie_release');
+const addMovieWatched = document.querySelector('#add-movie_watched');
+
+//opens interface to add Movies to list
+const addNewMovieButton = document.querySelector('#add-new-movie');
+const addNewMovieElem = document.querySelector('#add-movie');
+
+//Printout
+const moviesListElem = document.querySelector('#moviesList');
 
 async function addMovie(movie) {
     try {
@@ -12,14 +23,69 @@ async function addMovie(movie) {
     }
 };
 
+async function getMovies() {
+    // tar bort gamla listan
+    while (moviesListElem.hasChildNodes()) { 
+        moviesListElem.removeChild(moviesListElem.children[0]);
+    } 
+
+    // fyller på listan från vår databas
+    const moviesList = await getDocs(collection(db, 'movies')); 
+    moviesList.forEach((movies) => {
+        const movie = movies.data();
+        createMoviesElement(movie);
+    });
+}
+
+async function updateMovie(movie) {
+    //await updateDoc(doc(db, 'movies',movie.id));
+}
+
+async function deleteMovie(movie) {
+
+}
+
+function createMoviesElement(movie) {
+    const articleElem = document.createElement('article');
+    articleElem.setAttribute("class", "moviesList_movie");
+    const textElem = document.createElement('p');
+    textElem.innerText = `Title: ${movie.title} - Genre: ${movie.genre} - Release Date: ${movie.release}`;
+    const watchedElem = document.createElement('input');
+    watchedElem.setAttribute("type", "checkbox");
+    if (movie.watched === true) { watchedElem.setAttribute("checked", ""); } //fyller checkboxen bara om watched = true
+    console.log(movie.watched);
+    const deleteElem = document.createElement('button');
+    deleteElem.append("Delete");
+    moviesListElem.append(articleElem);
+    articleElem.append(textElem, watchedElem, deleteElem);
+    // console.log(query(collection(db, 'movies'), where('movies', '==', )));
+
+    // detta ska du göra
+    watchedElem.addEventListener('click', () => {
+        updateMovie(movie);
+    });
+
+    deleteElem.addEventListener('click', () => {
+        deleteMovie(movie);
+    });
+}
+
+getMovies();
+
 addMovieButton.addEventListener('click', () => {
     const movie = {
-        title: addMovieText.value,
-        genre: '',
-        release: '',
-        watched: false,
-        createdAt: new Date().toLocaleDateString()
+        title: addMovieTitle.value,
+        genre: addMovieGenre.value,
+        release: addMovieRelease.value,
+        watched: addMovieWatched.checked,
       }
+    addMovie(movie);
+    addNewMovieButton.classList.toggle('hidden');
+    addNewMovieElem.classList.toggle('hidden');
+    getMovies();
+});
 
-      addMovie(movie);
+addNewMovieButton.addEventListener('click', () => {
+    addNewMovieButton.classList.toggle('hidden');
+    addNewMovieElem.classList.toggle('hidden');
 });
